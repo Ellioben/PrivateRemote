03e43a0136eae756849ca3ab16e6ed2d6f6fb3a2
Yongbo,Xie <xieyongbo@bytedance.com>
2022-04-29 11:19:49 +0800

    features: fix filepath and py

---
+++ changelog/node/CHANGELOG.md
@@ -0,0 +1,5 @@
+# Version 0.0.1 (2022-04-29)
+
+## Features
+* 添加没用的sh脚本 ([3fec16](https://github.com/ObsidianXie/PrivateRemote/commit/3fec1624d463fa3a289c117c5e94953eb9ff6929))
+
--- changelog/node/generate.js
+++ changelog/node/generate.js
@@ -18,8 +18,6 @@ const currentChangelog = fs.readFileSync("./CHANGELOG.md", "utf-8");
 const currentVersion = String(require("./release_version.json").version);
 const commitPath = String(require("./release_version.json").origin);
 
-const newVersion = currentVersion + 1;
-
 // 用version和时间作为release 标记
 let newChangelog = `# Version ${currentVersion} (${
   new Date().toISOString().split("T")[0]
